bcf11f8a3bc7a26a73d6f8821bc5edf162fda29a
hegyhati <hegyhati.work@gmail.com>
2021-02-09 09:31:21 +0100

    Add constructors

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Course course = new Course();
+        Course course = new Course("Matematika 1", 4);
         System.out.println("Sikerult letrehozni az orat!");
     }
 }
--- ScheduledCourse.java
+++ ScheduledCourse.java
@@ -1,5 +1,10 @@
 public class ScheduledCourse extends Course {
     private int start;
+
+    public ScheduledCourse(String name, int duration, int start) {
+        super(name,duration);
+        this.start=start;
+    }
     
     public int getStart() { return start; }
     public int getEnd() { return start + getDuration(); }
