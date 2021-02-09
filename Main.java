f3e68996dcf0435aa208e5a2f48bc8df7461ebb1
hegyhati <hegyhati.work@gmail.com>
2021-02-09 08:57:27 +0100

    meh

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Course course = new Course();
-        System.out.println("Sikerult letrehozni az orat.");
+        System.out.println("Sikerult letrehozni az orat!");
     }
 }
