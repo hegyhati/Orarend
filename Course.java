c8eb29408f1af81973bbe714ea3d29b63b272907
hegyhati <hegyhati.work@gmail.com>
2021-02-09 08:52:02 +0100

    Add getter for duration at Course

--- Course.java
+++ Course.java
@@ -1,6 +1,7 @@
 public class Course {
     private String name;
     private int duration;
-    
+
     public String getName() { return name; }
+    public int getDuration() { return duration; }
 }
