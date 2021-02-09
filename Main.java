796a0f8fd10cbe55f4b2b79fef0d8289ae472abb
hegyhati <hegyhati.work@gmail.com>
2021-02-09 08:49:06 +0100

    Change c to course

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Course c = new Course();
+        Course course = new Course();
         System.out.println("Sikerult letrehozni az orat.");
     }
 }
