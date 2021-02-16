c41319caad564f50fc7329fce047b9aec756ce0d
hegyhati <hegyhati.work@gmail.com>
2021-02-16 08:38:53 +0100

    new interactive main

--- Main.java
+++ Main.java
@@ -1,12 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Course course = new Course("Matematika 1", 4);
-        System.out.println("Sikerult letrehozni az orat!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Mi a targy neve?");
+        String name = sc.next();
+        System.out.println("Hany oras aa targy?");
+        int duration = sc.nextInt();
+        Course c = new Course(name, duration);
+        System.out.println("Mikor kezdodjon az ora?");
+        int start = sc.nextInt();
+        ScheduledCourse s = new ScheduledCourse(c, start);
 
-        Course c = new Course("Algebra",2);
-        ScheduledCourse sc=new ScheduledCourse(c,4);
-
-        System.out.println(c);
-        System.out.println(sc);
+        System.out.println(s);
+        sc.close();
     }
 }
