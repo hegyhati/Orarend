354f06130435ad843587cfb2d9bc9ba0104f9614
hegyhati <hegyhati.work@gmail.com>
2021-02-16 09:03:18 +0100

    phrasing

--- Main.java
+++ Main.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Name?");
+        System.out.println("What is the name of the course?");
         String name = sc.next();
-        System.out.println("Duration?");
+        System.out.println("How many hours?");
         int duration = sc.nextInt();
         Course c = new Course(name, duration);
-        System.out.println("Start?");
+        System.out.println("When does it start?");
         int start = sc.nextInt();
         ScheduledCourse s = new ScheduledCourse(c, start);
 
