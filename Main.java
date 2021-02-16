8bdc8fd62cbf661187f512461477e55801ede9f2
hegyhati <hegyhati.work@gmail.com>
2021-02-16 08:53:04 +0100

    input in same line

--- Main.java
+++ Main.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Mi a targy neve?");
+        System.out.print("Mi a targy neve? ");
         String name = sc.next();
-        System.out.println("Hany oras aa targy?");
+        System.out.print("Hany oras aa targy? ");
         int duration = sc.nextInt();
         Course c = new Course(name, duration);
-        System.out.println("Mikor kezdodjon az ora?");
+        System.out.print("Mikor kezdodjon az ora? ");
         int start = sc.nextInt();
         ScheduledCourse s = new ScheduledCourse(c, start);
 
