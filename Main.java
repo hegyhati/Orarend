5ea0c79f8de1aa6fc0c39bab7fca0ad485d9c18c
hegyhati <hegyhati.work@gmail.com>
2021-02-23 08:13:22 +0100

    Collisions done close #2

--- Main.java
+++ Main.java
@@ -10,11 +10,31 @@ public class Main {
         int duration = sc.nextInt();
         Course c = new Course(name, duration);
         System.out.print("Start? ");
-
         int start = sc.nextInt();
         ScheduledCourse s = new ScheduledCourse(c, start);
-
         System.out.println(s);
+
+        System.out.print("Name? ");
+        name = sc.next();
+        System.out.print("Duration? ");
+        duration = sc.nextInt();
+        Course c2 = new Course(name, duration);
+        System.out.print("Start? ");
+        start = sc.nextInt();
+        ScheduledCourse s2 = new ScheduledCourse(c2, start);
+
+        
+        System.out.println(s);
+        System.out.println(s2);
+
+        if(s.overlapsWith(s2)){
+            System.out.println("They overlap.");
+        } else {
+            System.out.println("They don't overlap.");
+        }
+
+
+
         sc.close();
     }
 }
