6e8e110c054906283412a999166e4473146c6d00
hegyhati <hegyhati.work@gmail.com>
2021-03-09 08:33:49 +0100

    setup

--- ScheduledCourseTest.java
+++ ScheduledCourseTest.java
@@ -1,5 +1,8 @@
 
 public class ScheduledCourseTest {
+    static ScheduledCourse sc1;
+    static ScheduledCourse sc2;
+
     public static void main(String [] args){
         testOverlap();
         testtGetEnd();
@@ -14,16 +17,22 @@ public class ScheduledCourseTest {
         }
     }
 
+    static void setup(){
+        sc1=new ScheduledCourse("SZTA",4,8);
+        sc2=new ScheduledCourse("FoNyA",4,10);
+    }
+
     private static void testtGetEnd() {
         System.out.println("GETEND TEST");
+        setup();
         ScheduledCourse sc1=new ScheduledCourse("SZTA",4,8);
         assertEqual(sc1.getEnd(), 12, "Hibas getEnd");
+        assertEqual(sc2.getEnd(), 14, "Hibas getEnd");
     }
 
     private static void testOverlap() {
         System.out.println("OVERLAP TEST");
-        ScheduledCourse sc1=new ScheduledCourse("SZTA",4,8);
-        ScheduledCourse sc2=new ScheduledCourse("FoNyA",4,10);
+        setup();
         assertEqual(sc1.overlapsWith(sc1), true, "Hibas overlap");
         assertEqual(sc1.overlapsWith(sc2), true, "Hibas overlap");
         assertEqual(sc2.overlapsWith(sc1), true, "Hibas overlap");
