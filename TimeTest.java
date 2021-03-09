a2edfb3abdb0d91725b33282d96a56247e8787e3
hegyhati <hegyhati.work@gmail.com>
2021-03-09 08:59:24 +0100

    More test & displayname

--- TimeTest.java
+++ TimeTest.java
@@ -1,12 +1,22 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Assertions;
 
 public class TimeTest {
     
     @Test
+    @DisplayName("Test for int constructor")
     void testIntConstructor(){
         Time t=new Time(123);
-        Assertions.assertEquals(t.getHours(),2,"Wrong hours for int constructor.");
-        Assertions.assertEquals(t.getMinutes(),3,"Wrong minutes for int constructor.");
+        Assertions.assertEquals(2,t.getHours(),"Wrong hours for int constructor.");
+        Assertions.assertEquals(3,t.getMinutes(),"Wrong minutes for int constructor.");
+    }
+
+    @Test
+    @DisplayName("Test for int,int constructor")
+    void testIntIntConstructor(){
+        Time t=new Time(4,20);
+        Assertions.assertEquals(4,t.getHours(),"Wrong hours for int constructor.");
+        Assertions.assertEquals(20,t.getMinutes(),"Wrong minutes for int constructor.");
     }
 }
